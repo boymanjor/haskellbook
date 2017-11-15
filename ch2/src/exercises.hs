9f23e5295c073648d2f3b3fb3dba3ad9164b170e
Boyma Fahnbulleh <boymanjor+github@protonmail.com>
2017-11-15 13:23:27 -0800

    ch2: include notes and exercises

---
+++ ch2/src/exercises.hs
@@ -0,0 +1,23 @@
+module Exercises where
+
+-- Comprehension Check
+-- 1. Given the following lines of code as they might appear in a source file,
+--    how would you change them to use them directly in the REPL?
+--
+--    q: half x = x / 2
+--    a: let half x = x / 2
+--
+--    q: square x = x * x
+--    a: let square x = x * x
+
+-- 2. Write one function that has one parameter and works for all the following
+--    expression. Be sure to name the function.
+--
+--    3.14 * (5 * 5)
+--    3.14 * (10 * 10)
+--    etc.
+byPi1 x = x * 3.14
+
+-- 3. There is a value in Prelude called pi. Rewrite your function to use pi instead
+--    of 3.14
+byPi2 x = x * pi
