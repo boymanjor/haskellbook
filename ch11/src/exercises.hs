d2eb124f2fba2c951874b61d63fba88225c0d3c4
Boyma Fahnbulleh <boymanjor+github@protonmail.com>
2017-12-11 15:23:29 -0800

    ch11: finish exercises

--- ch11/src/exercises.hs
+++ ch11/src/exercises.hs
@@ -1,6 +1,8 @@
 module Exercises where
 
 import Data.Int
+import Data.Char
+import Data.List
 
 -- Dog Types --
 --
@@ -207,6 +209,156 @@ isSubseqOf xs'@(x:xs) ys'@(y:ys)
 
 -- 2. Split a sentence into words, then tuple each word with the capitalized version
 --    of it.
-capitalizeWords :: String
-                -> [(String, String)]
-capitalizeWords = undefined
+--
+-- Call words on the string
+-- Call map with [String] -> [(String, String)]
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords = map transform . words
+  where
+    transform cs'@(c:cs) = (cs', toUpper c : cs)
+
+-- Language exercises --
+--
+-- 1. Write a function that capitalizes a word.
+capitalizeWord :: String -> String
+capitalizeWord (c:cs) = toUpper c : cs
+
+-- 2. Write a function that capitalizes sentences in a paragraph.
+--
+-- Split string based on "."
+-- Call map with capitalizeWord
+-- Append period to each String.
+-- Call concat on the list.
+capitalizeParagraph :: String -> String
+capitalizeParagraph = unwords . map transform . map capitalizeWord . split
+  where
+    split cs
+      | cs == ""  = []
+      | otherwise = hd : split tl
+      where
+        hd = takeWhile (/= '.') . dropWhile (== '.') . dropWhile (== ' ') $ cs
+        tl = dropWhile (== '.') . dropWhile (/= '.') . dropWhile (== '.') $ cs
+
+    transform cs = cs ++ "."
+
+-- Phone exercises --
+--
+-- 1 Create a data structure that captures the phone layout.
+type Digit = Char
+type Characters = [Char]
+type Presses = Int
+
+data Button = Button Digit Characters
+type DaPhone = [Button]
+
+phone :: DaPhone
+phone =
+  [Button '1' "1",
+   Button '2' "abc2",
+   Button '3' "def3",
+   Button '4' "ghi4",
+   Button '5' "jkl5",
+   Button '6' "mno6",
+   Button '7' "pqrs7",
+   Button '8' "tuv8",
+   Button '9' "wxyz9",
+   Button '0' "+ 0",
+   Button '*' "^*",
+   Button '#' ".,#"]
+
+-- 2. Convert the following conversations into the keypresses
+--    required to express them.
+convo :: [String]
+convo =
+  ["Wanna play 20 questions",
+   "Ya",
+   "U 1st haha",
+   "Lol ok. Have u ever tasted alcohol",
+   "Lol ya",
+   "Ok. Do u think I am pretty Lol",
+   "Lol ya",
+   "Just making sure rofl ur turn"]
+
+reverseTaps :: DaPhone
+            -> Char
+            -> [(Digit, Presses)]
+reverseTaps phone c = foldr fold [] phone
+  where
+    fold (Button digit chars) acc
+      | isUpper c    = ('*' :: Digit, 1 :: Presses) : reverseTaps phone (toLower c)
+      | elem c chars = [(digit, getIndex . elemIndex c $ chars)]
+      | otherwise    = acc
+
+    getIndex (Just i) = (i + 1) :: Presses
+
+cellPhonesDead :: DaPhone
+               -> String
+               -> [(Digit, Presses)]
+cellPhonesDead phone cs = foldr fold [] cs
+  where
+    fold c acc = concat [reverseTaps phone c, acc]
+
+keypresses :: [[(Digit, Presses)]]
+keypresses = map (cellPhonesDead phone) convo
+
+-- 3. How many times do digits need to be pressed for each message?
+fingerTaps :: [(Digit, Presses)] -> Presses
+fingerTaps = length
+
+dpm :: [Presses]
+dpm = map fingerTaps keypresses
+
+-- 4. What was the most popular letter for each messsage?
+-- sort string
+-- recurse to find most popular letter
+--   - pattern match on cs'@(c:cs)
+--   - find # of current character: takeWhile (==c) cs 
+--   - check if this number is larger than current max
+--   - recurse with dropWhile (==c) and current max
+mostPopularTuple :: [Char] -> (Char, Presses)
+mostPopularTuple cs = go (sort cs) (' ', 0)
+  where
+    go [] tup = tup
+    go cs'@(c:_) (maxc, maxp)
+      | cnt > maxp = go next (c, cnt)
+      | otherwise  = go next (maxc, maxp)
+      where
+        cnt  = length . takeWhile (==c) $ cs'
+        next = dropWhile (==c) cs'
+
+mostPopularLetter :: String -> Char
+mostPopularLetter = fst . mostPopularTuple
+
+cost :: String -> Presses
+cost = fingerTaps . reverseTaps phone . mostPopularLetter
+
+-- 5. What was the most popular letter overall? What was the most popular
+--    word?
+coolestLtr :: [String] -> Char
+coolestLtr = mostPopularLetter . concat
+
+coolestWord :: [String] -> String
+coolestWord = fst . go (" ", 0) . words . unwords
+  where
+    go tup [] = tup
+    go (maxc, maxp) cs'@(c:_)
+      | cnt > maxp = go (c, cnt) next
+      | otherwise  = go (maxc, maxp) next
+      where
+        cnt  = length . takeWhile (==c) $ cs'
+        next = dropWhile (==c) cs'
+
+-- Hutton's Razor --
+data Expr
+  = Lit Integer
+  | Add Expr Expr
+
+-- 1. Write an eval function which reduces an expression to a final sum.
+eval :: Expr -> Integer
+eval (Lit n) = n
+eval (Add e1 e2) = (+) (eval e1) (eval e2)
+
+-- 2. Write a printer for the expresssions.
+printExpr :: Expr -> String
+printExpr (Lit n) = show n
+printExpr (Add e1 e2) = concat ["(", printExpr e1, " + ", printExpr e2, ")"]
