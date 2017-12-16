d7a84a24177ca438c620fd3e73c0ba1bb1992155
Boyma Fahnbulleh <boymanjor+github@protonmail.com>
2017-12-16 15:36:10 -0800

    ch13/src: finish chapter exercises

--- ch13/src/exercises.hs
+++ ch13/src/exercises.hs
@@ -1,5 +1,11 @@
 module Exercises where
 
+import Control.Monad (forever)
+import Data.Char
+import Text.Read
+import System.IO
+import System.Exit (exitSuccess)
+
 -- Intermissionn: Check your understanding
 --
 -- import qualified Control.Concurrent as CC
@@ -39,3 +45,76 @@ module Exercises where
 --
 --    c) Which import did forever come from?
 --    Answer: Control.Monad
+--
+-- Chapter Exercises --
+--
+-- 1. Can be found in ../../lib/cipher.hs
+--
+-- 2. Modify palindrome to exit with a false result.
+palindrome :: IO ()
+palindrome = forever $ do
+  line1 <- getLine
+  case (line1 == reverse line1) of
+    True  -> putStrLn "It's a palindrome!"
+    False -> do
+      putStrLn "Nope!"
+      exitSuccess
+
+-- 3. Modify palindrome to work for sentences.
+palindrome' :: IO ()
+palindrome' = forever $ do
+  line1 <- getLine
+  let line2 = transform line1
+  case (line2 == reverse line2) of
+    True  -> putStrLn "It's a palindrome!"
+    False -> do
+      putStrLn "Nope!"
+      exitSuccess
+  where
+    transform cs = [ toLower c | c <- cs, isAlpha c]
+
+-- 4. Given the following code, finish the undefined function.
+type Name = String
+type Age = Integer
+
+data Person = Person Name Age deriving Show
+
+data PersonInvalid =
+    NameEmpty
+  | AgeTooLow
+  | PersonInvalidUnknown String
+  deriving (Eq, Show)
+
+mkPerson :: Name
+         -> Age
+         -> Either PersonInvalid Person
+mkPerson name age
+  | name /= "" && age > 0 =
+      Right $ Person name age
+  | name == "" = Left NameEmpty
+  | not (age > 0) = Left AgeTooLow
+  | otherwise =
+      Left $ PersonInvalidUnknown $
+        "Name was: " ++ show name ++
+        "Age was: " ++ show age
+
+gimmePerson :: IO ()
+gimmePerson = do
+  hSetBuffering stdout NoBuffering
+  putStr "Enter name: "
+  name <- getLine
+  putStr "Enter age: "
+  mAge <- getLine
+  case readMaybe mAge :: Maybe Integer of
+    Just age -> do
+      let person = mkPerson name age
+      case person of
+        Left p -> do
+          putStrLn $ "Error: " ++ show p
+          return ()
+        Right p -> do
+          putStrLn $ "Yay! Successfully got a person: " ++ show p
+          return ()
+    Nothing -> do
+      putStrLn "Age must be an integer."
+      return ()
