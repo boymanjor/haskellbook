8a2eeefee92c41cbb163eef0c381549ff289c655
Boyma Fahnbulleh <boymanjor+github@protonmail.com>
2017-12-18 17:04:13 -0800

    ch14/exercises: rename wordnumber project

---
+++ ch14/exercises/src/WordNumber.hs
@@ -0,0 +1,27 @@
+module WordNumber where
+
+import Data.List (intersperse)
+
+digitToWord :: Int -> String
+digitToWord n
+  | n == 0    = "zero"
+  | n == 1    = "one"
+  | n == 2    = "two"
+  | n == 3    = "three"
+  | n == 4    = "four"
+  | n == 5    = "five"
+  | n == 6    = "six"
+  | n == 7    = "seven"
+  | n == 8    = "eight"
+  | n == 9    = "nine"
+  | otherwise = "N/A"
+
+digits :: Int -> [Int]
+digits n = go n []
+  where go num list
+          | num < 10  = (num:list)
+          | otherwise = go (div num 10) ((mod num 10):list)
+
+wordNumber :: Int -> String
+wordNumber n =
+  concat . intersperse "-" . map digitToWord . digits $ n
