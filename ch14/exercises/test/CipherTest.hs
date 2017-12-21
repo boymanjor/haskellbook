a87f87433cdc70cf0f649430c1f80bc054c767c9
Boyma Fahnbulleh <boymanjor+github@protonmail.com>
2017-12-20 16:30:55 -0800

    ch14/exercises: finish exercises and add follow-up resources

--- ch14/exercises/test/CipherTest.hs
+++ ch14/exercises/test/CipherTest.hs
@@ -1,10 +1,28 @@
 module CipherTest where
 
+import Data.Char
 import Cipher
+import Test.QuickCheck
 
-prop_caesar
+genString :: Gen String
+genString = (arbitrary :: Gen String) `suchThat` (all isAscii)
+
+genNonNeg :: Gen Int
+genNonNeg = (arbitrary :: Gen Int) `suchThat` (>=0)
+
+-- caesar :: Int -> String -> String
+prop_caesar :: Property
+prop_caesar =
+  forAll genNonNeg $ \n ->
+  forAll genString $ \xs -> (uncaesar n . caesar n $ xs) == xs
+
+-- vigenere :: String -> String -> String
+prop_vigenere :: Property
+prop_vigenere =
+  forAll genString $ \ks ->
+  forAll genString $ \ps -> (unvigenere ks . vigenere ks $ ps) == ps
 
 main :: IO ()
 main = do
-  quickCheck
-  quickCheck
+  quickCheck prop_caesar
+  quickCheck prop_vigenere
